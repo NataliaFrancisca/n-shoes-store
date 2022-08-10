62688b72ad520eee31f92d9627783d357028da08
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-10 14:59:21 -0300

    update images object

--- database.js
+++ database.js
@@ -7,12 +7,12 @@ const data = [
         preco: 549, 
         textDetails: "Comemore o 'Dia dos Mortos' mexicano com estampas festivas de caveira no Chuck 70 premium.",
         image: "/images/converse-dia-muertos-1.jpg",
-        images: [
-            {url: "/images/converse-dia-muertos-1.jpg"},
-            {url: "/images/converse-dia-muertos-2.jpg"},
-            {url: "/images/converse-dia-muertos-3.jpg"},
-            {url: "/images/converse-dia-muertos-4.jpg"},
-        ]
+        images: {
+            first: "/images/converse-dia-muertos-1.jpg",
+            second: "/images/converse-dia-muertos-2.jpg",
+            thirth: "/images/converse-dia-muertos-3.jpg",
+            fourth: "/images/converse-dia-muertos-4.jpg",
+        }
     },
 
     {
@@ -23,13 +23,12 @@ const data = [
         preco: 455, 
         textDetails: "Wear your heart on your Chucks. These high top Chuck 70 sneakers combine the unmistakable Chuck Taylor features you know and love with lovingly embroidered details and unexpected kiss prints at the outsole.",
         image: "/images/Chuck-70-Embroidered-Lips-1.jpg",
-        images: [
-            {url: "images/Chuck-70-Embroidered-Lips-1.jpg"},
-            {url: "images/Chuck-70-Embroidered-Lips-2.jpg"},
-            {url: "images/Chuck-70-Embroidered-Lips-3.jpg"},
-            {url: "images/Chuck-70-Embroidered-Lips-4.jpg"},
-
-        ]
+        images: {
+            first: "/images/Chuck-70-Embroidered-Lips-1.jpg",
+            second: "/images/Chuck-70-Embroidered-Lips-2.jpg",
+            thirth: "/images/Chuck-70-Embroidered-Lips-3.jpg",
+            fourth: "/images/Chuck-70-Embroidered-Lips-4.jpg",
+        }
     },
 
     {
@@ -40,12 +39,12 @@ const data = [
         preco: 245, 
         textDetails: "Some days call for glitter, and these Chuck 70s deliver. A two-tone design of shimmering canvas adds shine to any outfit, whether you're off to a holiday party or dressing up your everyday look.",
         image: "/images/Authentic-Glam-Chuck-70-1.jpg",
-        images: [
-            {url: "/images/Authentic-Glam-Chuck-70-1.jpg"},
-            {url: "/images/Authentic-Glam-Chuck-70-2.jpg"},
-            {url: "/images/Authentic-Glam-Chuck-70-3.jpg"},
-            {url: "/images/Authentic-Glam-Chuck-70-4.jpg"},
-        ]
+        images: {
+            first: "/images/Authentic-Glam-Chuck-70-1.jpg",
+            second: "/images/Authentic-Glam-Chuck-70-2.jpg",
+            thirth: "/images/Authentic-Glam-Chuck-70-3.jpg",
+            fourth: "/images/Authentic-Glam-Chuck-70-4.jpg",
+        }
     },
 
     {
@@ -56,12 +55,12 @@ const data = [
         preco: 650, 
         textDetails: "A chunky platform and jagged rubber sole put an unexpected twist on your everyday Chucks.",
         image: "/images/Run-Star-Hike-1.jpg",
-        images: [
-            {url: "/images/Run-Star-Hike-1.jpg"},
-            {url: "/images/Run-Star-Hike-2.jpg"},
-            {url: "/images/Run-Star-Hike-3.jpg"},
-            {url: "/images/Run-Star-Hike-4.jpg"},
-        ]
+        images: {
+            first: "/images/Run-Star-Hike-1.jpg",
+            second: "/images/Run-Star-Hike-2.jpg",
+            thirth: "/images/Run-Star-Hike-3.jpg",
+            fourth: "/images/Run-Star-Hike-3.jpg",
+        }
     },
 ]
 
